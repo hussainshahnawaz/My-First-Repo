c9677692806a482f0d78cdab48beb94ae9ae0422
hussainshahnawaz <49582872+hussainshahnawaz@users.noreply.github.com>
2021-05-18 19:45:16 +0530

    Add files via upload
    Chinmaya Mission Hospitals are often referred to as the best hospitals for the poorest and middle-class householders. With a new set of funds coming in, the CEO of Chinmaya mission Hospital has decided to open new branches in various parts of Karnataka. However, he doesn't have any data to target places that could be most beneficial to people. He has reached out to us to design an application that can record patients' locations. He wants to understand, on a given day or for a range of days, how many OP's are within Bangalore and how many are from outside. Based on this, he will make a decision if it is feasible to open new branches outside of Bangalore or open new Centers within Bangalore itself?

---
+++ decision.java
@@ -0,0 +1,81 @@
+package shahn;
+
+
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import shahn.Patient;
+
+public class decision {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		
+		int bcount=0,ocount=0;
+		String sDate1="13/06/2021";  
+		String sDate2="16/06/2021";  
+
+	    Date from=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);  
+	    Date to=new SimpleDateFormat("dd/MM/yyyy").parse(sDate2);  
+	    
+		List<Patient> allPatient = new ArrayList<shahn.Patient>();
+
+	    Patient p=new Patient();
+	    p.setName("shahnawaz");
+	    p.setlocation("jharkhand");
+	    p.setdate1(new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+	    allPatient.add(p);
+	    Patient p2=new Patient();
+	    p2.setName("kame");
+	    p2.setlocation("asam");
+	    p2.setdate1(new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+	    allPatient.add(p2);
+	    Patient p3=new Patient();
+	    p3.setName("sayeedur");
+	    p3.setlocation("mangalore");
+	    p3.setdate1(new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+	    allPatient.add(p3);
+	    Patient p4=new Patient();
+	    p4.setName("abhishekh");
+	    p4.setlocation("Bangalore");
+	    p4.setdate1(new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+	    allPatient.add(p4);
+	    Patient p5=new Patient();
+	    p5.setName("naresh");
+	    p5.setlocation("Bangalore");
+	    p5.setdate1(new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+	    allPatient.add(p5);
+	    
+	    int sizeAll=allPatient.size();
+	    for(int i=0;i<allPatient.size();i++){
+	    	if(allPatient.get(i).getlocation()=="Bangalore"){
+	    		if(allPatient.get(i).getdate1().compareTo(from)>0 && allPatient.get(i).getdate1().compareTo(to)<0){
+	    			bcount++;
+	    		}
+	    		else{
+	    		sizeAll--;
+	    		}
+	    	}else{
+		    	ocount++;
+	    	}
+	    }
+	    
+	    System.out.println(" Patient From Bangalore are  "+bcount);
+	    System.out.println(" Patient From outside of Bangalore are  "+ocount);
+	    
+	    
+	    Double inPatientPercentage = Double.valueOf(bcount * 100 / sizeAll);
+        Double outPatientPercentage = Double.valueOf(ocount * 100 / sizeAll);
+	    System.out.println("Percentage of total patient  from bangalore  "+inPatientPercentage);
+	    System.out.println("Percentage of total patient  from outside of bangalore  "+outPatientPercentage);
+
+	}
+
+}
+
